0253b341003efdaff00b606d905a567306221efe
tanikaradia <tanishka.karadia@gmail.com>
2025-05-30 15:19:18 +0530

    Added workout details to home page

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+const cors = require("cors"); //---| alt way for proxy
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+app.use(cors()); //---|
 
 //routes
 app.use("/api/workouts", workoutRoutes);
