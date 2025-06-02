164634fcd88272e6b27b925e45ab03d5decf0f7c
tanikaradia <tanishka.karadia@gmail.com>
2025-06-03 01:07:58 +0530

    added the workout form comp on home page

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -3,16 +3,55 @@ import { useState } from "react";
 const WorkoutForm = () => {
   const [title, setTitle] = useState("");
   const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //without this You type info and hit submit--Page refreshes 😩--Your React state is lost
+    const workout = { title, reps };
+
+    const response = await fetch("/api/workouts", {
+      //connect to BE
+      method: "POST",
+      body: JSON.stringify(workout), //body converted into JSON string
+      headers: {
+        "Content-Type": "application/json", //👈 “I’m sending JSON.”
+      },
+    }); //req
+    const json = await response.json(); //res
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+    if (response.ok) {
+      setTitle("");
+      setReps("");
+      setError(null);
+      console.log("new workout added", json);
+    }
+  };
 
   return (
-    <form className="create">
+    <form className="create" onSubmit={handleSubmit}>
       <h3>Add new Workout</h3>
       <label>Exercise Title:</label>
       <input
         type="text"
         onChange={(e) => setTitle(e.target.value)}
+        //e=event--passed to funn,target=triggered--ele causing event,value=current value typed
         value={title}
       />
+
+      <label>Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+      <button>Add Workout</button>
+
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
+
+export default WorkoutForm;
