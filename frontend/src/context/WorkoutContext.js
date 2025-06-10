e199f5ebf874610e7cc01a60a7178f4a33dc2365
tanikaradia <tanishka.karadia@gmail.com>
2025-06-10 23:36:08 +0530

    feat:setup workout context and reducer

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,36 @@
+import { createContext, useReducer } from "react";
+
+// 💡 useReducer + Context = Redux-lite
+export const WorkoutsContext = createContext(); //CONTEXTobj=funn() //Data container=ek global jagah banana jahan shared data rakha ja sake.
+//useContext(WorkoutsContext) tabhi kar paoge jab aapka component WorkoutsContextProvider ke andar ho.
+export const WorkoutsReducer = (state, action) => {
+  switch (
+    action.type //reducers==LOGIC
+  ) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUTS":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    default:
+      return state;
+  }
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  //namm kuchh bhi, but fir wahi App file me bhi use hoga
+
+  //Data supplier=context ko data dena via value={...}
+  const [state, dispatch] = useReducer(WorkoutsReducer, {
+    workouts: null,
+  });
+  return (
+    //state==CURRENT data, dispatch==funn to ACTION
+    <WorkoutsContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider> //App comp //⬆️createContext()
+  );
+};
